5e7cf096d00e5393b8523d46ce7722db4749dd86
njihiamark <njihiamark@gmail.com>
2020-07-28 18:20:26 +0300

    Test getSecretWord does NOT run on App update

--- src/App.test.js
+++ src/App.test.js
@@ -32,5 +32,14 @@ describe('getSecretWord calls', () => {
 
     //check to see if secret word was updated
     expect(mockGetSecretWord).toHaveBeenCalled();
+  });
+  test('secretWord does not update on App update', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+
+
+    wrapper.setProps();
+
+    expect(mockGetSecretWord).not.toHaveBeenCalled();
   })
 });
